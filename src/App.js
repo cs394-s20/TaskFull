fb18dccf3a10274c9d33a8b71f273c309277095a
Zev Stravitz <zevstravitz@verizon.net>
2020-04-10 10:34:47 -0400

    updated main page, added nav

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
-
+import { green, purple } from '@material-ui/core/colors';
+import NavBar from './components/NavBar'
+import Grid from '@material-ui/core/Grid';
 
 const exampleTasks = [
   {
     title: 'Grocery Store Run',
     author: 'Tracy Chapman',
+    description: 'I need help getting to the grocery store and carrying my bags.',
     location: '2110 CS Drive, Evanston, IL 60201',
     requirements: ['physical', 'car'],
     time: 'Thu, Apr 9, 2020 6:30 PM',
@@ -24,6 +25,7 @@ const exampleTasks = [
   {
     title: 'Dog Walking',
     author: 'Patrick Johnson',
+    description: 'I need someone to walk my dogs while my wife is sick.',
     location: '815 Green St, Evanston, IL 60201',
     requirements: ['pets'],
     time: 'Fri, Apr 17, 2020 2:00 PM',
@@ -33,6 +35,7 @@ const exampleTasks = [
   {
     title: 'Need help moving my couch',
     author: 'Drake Bell',
+    description: 'I\'m looking for someone to help me carry my couch out of my house. I can provide a facemask!',
     location: '565 Greenwood St, Evanston, IL 60201',
     requirements: ['physical', 'car'],
     time: 'Wed, Aug 9, 2020 6:30 PM',
@@ -42,6 +45,7 @@ const exampleTasks = [
   {
     title: 'Fixing a broken bike lock',
     author: 'Todd Meyers',
+    description: 'My bike lock seems to be broken and I cannot figure out how to fix it. Tried W-40.',
     location: '42 Blueberry Ln, Evanston, IL 60201',
     requirements: ['physical'],
     time: 'Fri, Apr 10, 2020 5:00 PM',
@@ -51,6 +55,7 @@ const exampleTasks = [
   {
     title: 'Garden maintenance',
     author: 'Ping',
+    description: 'I don\'t know anything about gardening but want to get started!',
     location: '832 Emerson St, IL 60201',
     requirements: ['physical'],
     time: 'Thu, Apr 9, 2020 11:30 AM',
@@ -66,21 +71,20 @@ const useStyles = makeStyles({
 });
 
 // This will be updated to have more than just a title!
-const TaskCard = (props) => {
-    const classes = useStyles();
-  
+const TaskCard = (props) => {  
     return (
       <Card className={'task-card'}>
-        <CardActionArea>
           <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
+            <Typography gutterBottom variant="h4" fontWeight={700}>
               {props.task.title}
+            </Typography>
+            <Typography gutterBottom variant="subtitle1">
+              {props.task.description}
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
               {props.task.time}
             </Typography>
           </CardContent>
-        </CardActionArea>
         <CardActions className="action-buttons">
           <Button size="small" color="primary">
             Accept
@@ -99,13 +103,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>
-          TaskFull Launch Page
-        </h2>
-        <p>an app that matches people in need with willing neighbors to help with errands</p>
-        {exampleTasks.map((task, index) => 
-          <TaskCard task={task} key={index}/>
-        )}
+        <NavBar></NavBar>
+        <Grid container spacing={0}>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={6}>
+          {exampleTasks.map((task, index) => (
+              <TaskCard task={task} key={index}/>
+          ))}
+          </Grid>
+        </Grid>
       </header>
     </div>
   );
