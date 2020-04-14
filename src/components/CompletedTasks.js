70515ff5d2262caaa25517b17f6e7e5434c9a7d2
Jordan Zax <jrzax@aol.com>
2020-04-13 20:40:02 -0400

    Fixed Duplicates in Completed Section

--- src/components/CompletedTasks.js
+++ src/components/CompletedTasks.js
@@ -9,6 +9,7 @@ const CompletedTasks = () =>{
 
     const filteredList = completeList.filter(t=>t.status==='complete');
     //NOTE: completeList has duplicates of tasks, uniqueList seems to be a temporary work around
+
     const uniqueList =Array.from(new Set(filteredList.map(a=>a.id))).map(id=>{
         return filteredList.find(a=>a.id===id)
     });
